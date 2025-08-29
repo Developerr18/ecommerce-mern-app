bdacb69dd1ae8d1e513fe27a88bac481a3f09ed8
Developerr18 <dineshnikum1@gmail.com>
2025-08-30 03:23:10 +0530

    completed: Add page UI and feature of adding product to DB

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -43,9 +43,7 @@ const Login = ({ setToken }) => {
             />
           </div>
           <div className="mb-3 min-w-72">
-            <p className="text-sm font-medium text-gray-700 mb-2">
-              Email Address
-            </p>
+            <p className="text-sm font-medium text-gray-700 mb-2">Password</p>
             <input
               className="rounded-md w-full px-3 py-2 border border-gray-300 outline-none"
               type="password"
@@ -56,7 +54,7 @@ const Login = ({ setToken }) => {
             />
           </div>
           <button
-            className="mt-2 w-full py-2 px-4 rounded-md text-white bg-black"
+            className="mt-2 w-full cursor-pointer py-2 px-4 rounded-md text-white bg-black"
             type="submit"
           >
             Login
--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = ({ setToken }) => {
       <img className="w-[10%] min-w-[80px]" src={assets.logo} />
       <button
         onClick={() => setToken("")}
-        className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm"
+        className="bg-gray-600 text-white cursor-pointer px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm"
       >
         Logout
       </button>
